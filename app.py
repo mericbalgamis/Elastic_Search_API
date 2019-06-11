6241ddf79841e19afae68fa8a2affc610c21d386
Meric <mericbalgamis@gmail.com>
2019-06-11 21:26:30 +0300

    CORS error

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 #!/usr/local/bin/python3
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-
-
-@app.route('/result', methods=['GET'])
-def send_result():
-    return jsonify({'result': 'This is result JSON'})
-
+#
+# @app.route('/result', methods=['GET'])
+# def send_result():
+#     return jsonify({'result': 'This is result JSON'})
+#
 
 @app.route('/query', methods=['POST'])
+@cross_origin()
 def get_query():
     print(request.is_json)
     content = request.get_json()
