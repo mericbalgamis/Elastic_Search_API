10fa07eee9654c92a693d98cfc8f7f06b76f49e0
mericbalgamis <mericbalgamis@gmail.com>
2019-07-23 19:03:51 +0300

    now success id field is correctly added to json

--- merge_json.py
+++ merge_json.py
@@ -1,34 +1,21 @@
-import requests, json, os
-import re
-from pprint import pprint
-from elasticsearch import Elasticsearch, helpers
-from tagDictionary import DcmTagDictionary
-from collections import defaultdict
 import json
-from pprint import pprint
 
-#00080018
 
 class OutputObject(object):
     def __init__(self, rule_id, SOP):
-        self.rule_id = rule_id,
+        self.rule_id = rule_id
         self.SOP = SOP
 
 
 name="1.2.752.24.7.2268657091.254554-e5141679-66c8-4f2f-97c0-2b7735e2ab79-2019-05-09_03-11-54"
 input_name = name+".json"
 output_name=name+"_test_out.json"
+output_list = []
 
 #rule result altındaki rule idlerin success edenleri sırayla gezilecek her birimnin sop uid degerine bakılarak
 # input jsonında hangi resme karşılık geldiği anlaşılacak ve oraya gömülecek
 
-
-
-
-   # f.write(json.dumps(json_list, indent=5, sort_keys=True))
-
-
-output_list = []
+#00080018
 
 
 # output json SOP_Instance_UID degerleri
@@ -42,8 +29,6 @@ with open(output_name) as json_file:
 
                 output_list.append(OutputObject(output_rule_id,output_SOP))
 
-#for obj in output_list:
-    #print(obj.rule_id)
 
 with open(input_name,"r+") as json_file:
     input = json.load(json_file)
@@ -51,50 +36,20 @@ with open(input_name,"r+") as json_file:
         for input_SOP in input['DCMs'][i]["00080018"]["Value"]:
             for output_obj in output_list:
                 if(input_SOP==output_obj.SOP):
+                    if('SUCCESS' in input['DCMs'][i]):
+                        input['DCMs'][i]['SUCCESS']['rule_id'].append(output_obj.rule_id)
+                        print(input['DCMs'][i]['SUCCESS']['rule_id'])
 
-                    a_dict = {'SUCCESS':{"rule_id":output_obj.rule_id}}
+                        with open(input_name, 'w') as f:
+                            json.dump(input, f)
 
+                    else:
+                        a_dict = {'SUCCESS': {"rule_id": [output_obj.rule_id]}}
+                        #a_dict = {'SUCCESS': {["rule_id": [output_obj.rule_id]}}
+                        input['DCMs'][i].update(a_dict)
 
-
-                    #print(output_obj.rule_id)
-
-                    #with open(input_name) as f:
-                        #data = json.load(f)
-
-                    input['DCMs'][i].update(a_dict)
-
-                    with open(input_name, 'w') as f:
-                          json.dump(input, f)
-
-
-
-
-
-
-
-    #for p in data['individual_results'][0]['rule_results']:
-            #print(json.dumps(output_SOP, indent=5, sort_keys=True))
-            #print(data)
-#    for i in range(0, 40):
-#        for k in data['individual_results'][0]['rule_results'][0]['SUCCESS'][i]["rule_id"]:#rule id
-#             print(k)
-
-
-#verilen tage göre input jsonı guncellemek  icin
-#a_dict = {'rule_id': 00}
-
-#with open(input_name) as f:
-#    data = json.load(f)
-
-#data.update(a_dict)
-#for p in data['DCMs']:
-#    with open(input_name, 'w') as f:
-#         json.dump(data, f)
-
-
-    # for p in data['DCMs']:
-    # print(json.dumps(p, indent=5, sort_keys=True))
-
+                        with open(input_name, 'w') as f:
+                            json.dump(input, f)
 
 
 
