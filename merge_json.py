50c50ca301f2177a18706e631118efdd7a20e9f1
Gökberk Usta <gokberk@Gokberks-MacBook-Pro.local>
2019-07-26 17:52:03 +0300

    input json merged with subset_id

--- merge_json.py
+++ merge_json.py
@@ -1,24 +1,28 @@
 import json
 
 class individualObject(object):
-    def __init__(self, rule_id, SOP):
+    def __init__(self, rule_id, SOP,subset_id):
         self.rule_id = rule_id
         self.SOP = SOP
+        self.subset_id = subset_id
 
 class mandatoryObject(object):
-    def __init__(self, rule_id, SOP):
+    def __init__(self, rule_id, SOP,subset_id):
         self.rule_id = rule_id
         self.SOP = SOP
+        self.subset_id = subset_id
 
 class optionalObject(object):
-    def __init__(self, rule_id, SOP):
+    def __init__(self, rule_id, SOP,subset_id):
         self.rule_id = rule_id
         self.SOP = SOP
+        self.subset_id = subset_id
 
 class recommendedObject(object):
-    def __init__(self, rule_id, SOP):
+    def __init__(self, rule_id, SOP,subset_id):
         self.rule_id = rule_id
         self.SOP = SOP
+        self.subset_id = subset_id
 
 #{
 #    "SUCCESS": {
@@ -68,8 +72,9 @@ with open(output_name) as json_file:
             for p in output['individual_results'][0]['rule_results'][0]['SUCCESS'][j]["SOP_Instance_UID"]["Value"]:
                 output_rule_id = output['individual_results'][0]['rule_results'][i]["rule_id"]
                 output_SOP = p
+                output_subset_id = output['individual_results'][0]['subset_id']
 
-                individual_list.append(individualObject(output_rule_id,output_SOP))
+                individual_list.append(individualObject(output_rule_id,output_SOP,output_subset_id))
 
 
 # output json bulk_result success degerleri mandatory
@@ -80,8 +85,9 @@ with open(output_name) as json_file:
             for p in output['bulk_results'][0]['mandatory']['SUCCESS'][i]['SUCCESS'][j]["SOP_Instance_UID"]["Value"]:
                 mandatory_rule_id=output['bulk_results'][0]['mandatory']['SUCCESS'][i]['SUCCESS'][j]["rule_id"]
                 mandatory_SOP_value= p
+                mandatory_subset_id = output['bulk_results'][0]['subset_id']
 
-                mandatory_list.append(mandatoryObject(mandatory_rule_id,mandatory_SOP_value))
+                mandatory_list.append(mandatoryObject(mandatory_rule_id,mandatory_SOP_value,mandatory_subset_id))
 
 
 # output json bulk_result success degerleri optional
@@ -92,8 +98,9 @@ with open(output_name) as json_file:
             for p in output['bulk_results'][0]['optional']['SUCCESS'][i]['SUCCESS'][j]["SOP_Instance_UID"]["Value"]:
                 optional_rule_id = output['bulk_results'][0]['optional']['SUCCESS'][i]['SUCCESS'][j]["rule_id"]
                 optional_SOP_value = p
+                optional_subset_id = output['bulk_results'][0]['subset_id']
 
-                optional_list.append(optionalObject(optional_rule_id, optional_SOP_value))
+                optional_list.append(optionalObject(optional_rule_id, optional_SOP_value,optional_subset_id))
 
 
 
@@ -105,8 +112,9 @@ with open(output_name) as json_file:
             for p in output['bulk_results'][0]['recommended']['SUCCESS'][i]['SUCCESS'][j]["SOP_Instance_UID"]["Value"]:
                 recommended_rule_id = output['bulk_results'][0]['recommended']['SUCCESS'][i]['SUCCESS'][j]["rule_id"]
                 recommended_SOP_value = p
+                recommended_subset_id = output['bulk_results'][0]['subset_id']
 
-                recommended_list.append(recommendedObject(recommended_rule_id, recommended_SOP_value))
+                recommended_list.append(recommendedObject(recommended_rule_id, recommended_SOP_value,recommended_subset_id))
 
 
 # input jsona SOP_Instance_UID degerlerine göre rule id lerin append edilmesi
@@ -115,52 +123,77 @@ with open(input_name,"r+") as json_file:
     for i in range(0, len(input['DCMs'])):
         for input_SOP in input['DCMs'][i]["00080018"]["Value"]:
 
+            b_dict = {
+                "SUCCESS": {
+                    "individual_results": {
+                        "subset_id": [
+
+                        ],
+                        "rule_id": [
+
+                        ]
+                    },
+                    "bulk_results": {
+                        "subset_id": {
+                        },
+                        "mandatory": {
+                            "rule_id": [
+                                ""
+                            ]
+                        },
+                        "optional": {
+                            "rule_id": [
+                                ""
+                            ]
+                        },
+                        "recommended": {
+                            "rule_id": [
+                                ""]
+                        }
+                    }
+                }
+            }
+            input['DCMs'][i].update(b_dict)
+
             for output_obj in individual_list:
                 if(input_SOP==output_obj.SOP):
-                    if('SUCCESS' in input['DCMs'][i]):
-                        input['DCMs'][i]['SUCCESS']['rule_id'].append(output_obj.rule_id)
-                       #print(input['DCMs'][i]['SUCCESS']['rule_id'])
+                    input['DCMs'][i]['SUCCESS']['individual_results']['rule_id'].append(output_obj.rule_id)
 
-                        with open(input_name, 'w') as f:
-                            json.dump(input, f)
+            input['DCMs'][i]['SUCCESS']['individual_results']['subset_id'].append(output_obj.subset_id)
 
-                    else:
-                        a_dict = {'SUCCESS': {"rule_id": [output_obj.rule_id]}}
-                        b_dict = {
-                            "SUCCESS": {
-                                "individual_results": {
-                                    "subset_id": [
-                                        output_obj.rule_id
-                                    ],
-                                   "rule_id": [
-                                       output_obj.rule_id
-                                    ]
-                                },
-                                "bulk_results": {
-                                    "subset_id": {
-                                        output_obj.rule_id
-                                    },
-                                   "mandatory": {
-                                        "rule_id": [
-                                            ""
-                                        ]
-                                    },
-                                    "optional": {
-                                        "rule_id": [
-                                            ""
-                                        ]
-                                   },
-                                    "recommended": {
-                                        "rule_id": [
-                                            ""]
+
+                       #input['DCMs'][i].update(b_dict)
+
+        '''
+                for output_obj in mandatory_list:
+                    if (input_SOP == output_obj.SOP):
+                        if ('bulk_results' in input['DCMs'][i]):
+                            input['DCMs'][i]['SUCCESS']['bulk_results']['mandatory']['rule_id'].append(output_obj.rule_id)
+                            print(output_obj.rule_id)
+
+                            with open(input_name, 'w') as f:
+                                json.dump(input, f)
+
+                        else:
+                            b_dict = {
+                                "SUCCESS": {
+                                    "bulk_results": {
+                                        "subset_id": [
+                                            output_obj.subset_id
+                                        ],
+                                        "mandatory":{
+                                            "rule_id": [
+                                                output_obj.rule_id
+                                            ]
+                                        }
                                     }
                                 }
                             }
-                         }
-                        input['DCMs'][i].update(a_dict)
+                            input['DCMs'][i].update(b_dict)
+                            '''
 
-                        with open(input_name, 'w') as f:
-                            json.dump(input, f)
+        with open(input_name, 'w') as f:
+            json.dump(input, f)
 
 
 
